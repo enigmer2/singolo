48177bced38cc3c21e7df682df8e6b6af3ade166
Datskevich Dzmitry <dda.enigmer@gmail.com>
2020-09-29 01:41:42 +0300

    fix: rebuild some html tags

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
 function menu_change() {
   $("#id-header-wrapper").toggleClass("header-wrapper header-wrapper-71px");
   $("#visible-menu").toggleClass("logo black-fixed-menu-visible");
-
-  $("#id-header-nav-wrapper").toggleClass(
-    "header-nav-wrapper black-fixed-nav-visible"
-  );
-  $("#id-grid-in-grid").toggleClass(
-    "nav-level-5-grid-in-grid black-fixed-nav-visible-link"
-  );
+  $("#id-header-nav-wrapper").toggleClass("header-nav-wrapper black-fixed-nav-visible");
+  $("#id-grid-in-grid").toggleClass("nav-level-5-grid-in-grid black-fixed-nav-visible-link");
   $("#id-h1-hidden").toggleClass("hidden black-out");
   $("#menu").toggleClass("nav-button black-fixed-menu-nav-button");
   $("#id-singolo").toggleClass("singolo singolo2");
-  $("li#id-nav-before-text").toggleClass(
-    "nav-before-text nav-before-text-hidden"
-  );
-  $(
-    "a#button-menu-home, a#button-menu-serv, a#button-menu-port, a#button-menu-cont"
-  ).toggleClass("open closed-menu-button ");
+  $("li#id-nav-before-text").toggleClass("nav-before-text nav-before-text-hidden");
+  $("a#button-menu-home, a#button-menu-serv, a#button-menu-port, a#button-menu-cont").toggleClass("open closed-menu-button ");
 }
 function menu_check() {
   if ($("#menu").attr("menu") == "closed") {
@@ -45,9 +36,7 @@ $("#shuffle, #shuffle2, #shuffle3, #shuffle4").click(function () {
 $("#menu, a[class='open']").click(() => {
   menu_check();
 });
-$(
-  "#button-menu-home, #button-menu-serv, #button-menu-port, #button-menu-cont"
-).click(() => {
+$("#button-menu-home, #button-menu-serv, #button-menu-port, #button-menu-cont").click(() => {
   if (
     $("#button-menu-home, #button-menu-serv, #button-menu-port, #button-menu-cont").attr("class") == "open") {
     menu_change();
